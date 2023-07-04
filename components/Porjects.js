85a16ccca87c826bd5425e087e78e1a55ccb2b1f
David-Mateus <david-mateus09@hotmail.com>
2023-07-04 08:18:18 -0300

    last update

--- components/Porjects.js
+++ components/Porjects.js
@@ -5,7 +5,7 @@ export default function Projects() {
            <div>
             <h3 className="text-3xl py-1 text-center dark:text-white ">Me</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-            Hello! I'm a junior developer with experience in an education-related project. My skills include Node.js, JavaScript, Next.js, Pixi.js, HTML5, CSS, SQL, and NoSQL (Oracle and MongoDB). I have experience in native mobile app development with Kotlin in Android Studio, and knowledge of ReactJS, React Native, and Python. I'm seeking opportunities to contribute to innovative projects.
+            Hello! Im a junior developer with experience in an education-related project. My skills include Node.js, JavaScript, Next.js, Pixi.js, HTML5, CSS, SQL, and NoSQL (Oracle and MongoDB). I have experience in native mobile app development with Kotlin in Android Studio, and knowledge of ReactJS, React Native, and Python. Im seeking opportunities to contribute to innovative projects.
             </p>
             
           </div>
@@ -15,10 +15,8 @@ export default function Projects() {
               <h3 className="text-lg font-medium pt-8 pb-2 text-white dark:text-gray-900 ">
                 Soft skills
               </h3>
-              <p className="py-2 text-white dark:text-gray-900">
-                lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+             
+              
               <p className="text-white dark:text-gray-900 py-1">Comunicação eficaz</p>
               <p className="text-white dark:text-gray-900 py-1">Trabalho em equipe</p>
               <p className="text-white dark:text-gray-900 py-1">Liderança</p>
@@ -29,10 +27,8 @@ export default function Projects() {
               <h3 className="text-lg font-medium pt-8 pb-2 text-white dark:text-gray-900 ">
                 Hard skills
               </h3>
-              <p className="py-2 text-white dark:text-gray-900">
-                lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+             
+              
               <p className="text-white dark:text-gray-900 py-1">JavaScript</p>
               <p className="text-white dark:text-gray-900 py-1">Python</p>
               <p className="text-white dark:text-gray-900 py-1">Kotlin</p>
@@ -42,15 +38,13 @@ export default function Projects() {
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 bg-gray-900  dark:bg-white flex-1">
               <Image src={"/consulting.png"} width={100} height={100} alt="ee"/>
-              <h3 className="text-lg font-medium pt-8 pb-2 text-white dark:text-gray-900 ">Consulting</h3>
-              <p className="py-2 text-white dark:text-gray-900">
-                lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-white dark:text-gray-900 py-1"></p>
-              <p className="text-white dark:text-gray-900 py-1"></p>
-              <p className="text-white dark:text-gray-900 py-1"></p>
-              <p className="text-white dark:text-gray-900 py-1"></p>
+              <h3 className="text-lg font-medium pt-8 pb-2 text-white dark:text-gray-900 ">Objective</h3>
+              
+              
+              <p className="text-white dark:text-gray-900 py-1">Improve problem solving skills</p>
+              <p className="text-white dark:text-gray-900 py-1">Deepen knowledge in specific areas</p>
+              <p className="text-white dark:text-gray-900 py-1">Improve code quality</p>
+              <p className="text-white dark:text-gray-900 py-1">Build meaningful projects</p>
             </div>
             
           </div>
